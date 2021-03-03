corsicanu/android_kernel_samsung_universal9820

selinux: reduce calls to context_struct_to_string()
context_struct_to_string() contains expensive kmalloc() calls.

In most cases, there's no purpose in calling context_struct_to_string()
on !CONFIG_AUDIT as logs won't be saved anyways.

Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

security/selinux/ss/services.c
@@ -456,6 +456,7 @@ mls_ops:
 	return s[0];
 }
 
+#ifdef CONFIG_AUDIT
 /*
  * security_dump_masked_av - dumps masked permissions during
  * security_compute_av due to RBAC, MLS/Constraint and Type bounds.
@@ -545,6 +546,7 @@ out:
 
 	return;
 }
+#endif
 
 /*
  * security_boundary_permission - drops violated permissions
@@ -598,9 +600,11 @@ static void type_attribute_bounds_av(struct context *scontext,
 	/* mask violated permissions */
 	avd->allowed &= ~masked;
 
+#ifdef CONFIG_AUDIT
 	/* audit masked permissions */
 	security_dump_masked_av(scontext, tcontext,
 				tclass, masked, "bounds");
+#endif
 }
 
 /*
@@ -737,11 +741,12 @@ static void context_struct_compute_av(struct context *scontext,
 				 tclass, avd);
 }
 
-static int security_validtrans_handle_fail(struct context *ocontext,
+static inline int security_validtrans_handle_fail(struct context *ocontext,
 					   struct context *ncontext,
 					   struct context *tcontext,
 					   u16 tclass)
 {
+#ifdef CONFIG_AUDIT
 	char *o = NULL, *n = NULL, *t = NULL;
 	u32 olen, nlen, tlen;
 
@@ -759,6 +764,7 @@ out:
 	kfree(o);
 	kfree(n);
 	kfree(t);
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -919,6 +925,7 @@ int security_bounded_transition(u32 old_sid, u32 new_sid)
 		index = type->bounds;
 	}
 
+#ifdef CONFIG_AUDIT
 	if (rc) {
 		char *old_name = NULL;
 		char *new_name = NULL;
@@ -938,6 +945,7 @@ int security_bounded_transition(u32 old_sid, u32 new_sid)
 		kfree(new_name);
 		kfree(old_name);
 	}
+#endif
 out:
 	read_unlock(&policy_rwlock);
 
@@ -1525,12 +1533,13 @@ int security_context_to_sid_force(const char *scontext, u32 scontext_len,
 					    sid, SECSID_NULL, GFP_KERNEL, 1);
 }
 
-static int compute_sid_handle_invalid_context(
+static inline int compute_sid_handle_invalid_context(
 	struct context *scontext,
 	struct context *tcontext,
 	u16 tclass,
 	struct context *newcontext)
 {
+#ifdef CONFIG_AUDIT
 	char *s = NULL, *t = NULL, *n = NULL;
 	u32 slen, tlen, nlen;
 
@@ -1550,6 +1559,7 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -1846,8 +1856,10 @@ static int clone_sid(u32 sid,
 
 static inline int convert_context_handle_invalid_context(struct context *context)
 {
+#ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -1857,10 +1869,12 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	if (selinux_enforcing)
 		return -EINVAL;
 
+#ifdef CONFIG_AUDIT
 	if (!context_struct_to_string(context, &s, &len)) {
 		printk(KERN_WARNING "SELinux:  Context %s would be invalid if enforcing\n", s);
 		kfree(s);
 	}
+#endif
 	return 0;
 }
 
@@ -1888,7 +1902,9 @@ static int convert_context(u32 key,
 	struct type_datum *typdatum;
 	struct user_datum *usrdatum;
 	char *s;
+#ifdef CONFIG_AUDIT
 	u32 len;
+#endif
 	int rc = 0;
 
 	if (key <= SECINITSID_NUM)
@@ -2002,6 +2018,7 @@ static int convert_context(u32 key,
 out:
 	return rc;
 bad:
+#ifdef CONFIG_AUDIT
 	/* Map old representation to string and save it. */
 	rc = context_struct_to_string(&oldc, &s, &len);
 	if (rc)
@@ -2014,6 +2031,9 @@ bad:
 	       c->str);
 	rc = 0;
 	goto out;
+#else
+    return 0;
+#endif
 }
 
 static void security_load_policycaps(void)
@@ -2869,8 +2889,10 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 	struct context *context1;
 	struct context *context2;
 	struct context newcon;
+#ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
+#endif
 	int rc;
 
 	rc = 0;
@@ -2910,6 +2932,7 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 	if (!policydb_context_isvalid(&policydb, &newcon)) {
 		rc = convert_context_handle_invalid_context(&newcon);
 		if (rc) {
+#ifdef CONFIG_AUDIT
 			if (!context_struct_to_string(&newcon, &s, &len)) {
 				audit_log(current->audit_context,
 					  GFP_ATOMIC, AUDIT_SELINUX_ERR,
@@ -2917,6 +2940,7 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 					  "invalid_context=%s", s);
 				kfree(s);
 			}
+#endif
 			goto out_unlock;
 		}
 	}

corsicanu/android_kernel_samsung_universal9830

security/selinux/ss/services.c
@@ -447,6 +447,7 @@ mls_ops:
 	return s[0];
 }
 
+#ifdef CONFIG_AUDIT
 /*
  * security_dump_masked_av - dumps masked permissions during
  * security_compute_av due to RBAC, MLS/Constraint and Type bounds.
@@ -537,6 +538,7 @@ out:
 
 	return;
 }
+#endif
 
 /*
  * security_boundary_permission - drops violated permissions
@@ -591,9 +593,11 @@ static void type_attribute_bounds_av(struct policydb *policydb,
 	/* mask violated permissions */
 	avd->allowed &= ~masked;
 
+#ifdef CONFIG_AUDIT
 	/* audit masked permissions */
 	security_dump_masked_av(policydb, scontext, tcontext,
 				tclass, masked, "bounds");
+#endif
 }
 
 /*
@@ -734,12 +738,13 @@ static void context_struct_compute_av(struct policydb *policydb,
 				 tclass, avd);
 }
 
-static int security_validtrans_handle_fail(struct selinux_state *state,
+static inline int security_validtrans_handle_fail(struct selinux_state *state,
 					   struct context *ocontext,
 					   struct context *ncontext,
 					   struct context *tcontext,
 					   u16 tclass)
 {
+#ifdef CONFIG_AUDIT
 	struct policydb *p = &state->ss->policydb;
 	char *o = NULL, *n = NULL, *t = NULL;
 	u32 olen, nlen, tlen;
@@ -758,6 +763,7 @@ out:
 	kfree(o);
 	kfree(n);
 	kfree(t);
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -936,6 +942,7 @@ int security_bounded_transition(struct selinux_state *state,
 		index = type->bounds;
 	}
 
+#ifdef CONFIG_AUDIT
 	if (rc) {
 		char *old_name = NULL;
 		char *new_name = NULL;
@@ -955,6 +962,7 @@ int security_bounded_transition(struct selinux_state *state,
 		kfree(new_name);
 		kfree(old_name);
 	}
+#endif
 out:
 	read_unlock(&state->ss->policy_rwlock);
 
@@ -1585,13 +1593,14 @@ int security_context_to_sid_force(struct selinux_state *state,
 					    sid, SECSID_NULL, GFP_KERNEL, 1);
 }
 
-static int compute_sid_handle_invalid_context(
+static inline int compute_sid_handle_invalid_context(
 	struct selinux_state *state,
 	struct context *scontext,
 	struct context *tcontext,
 	u16 tclass,
 	struct context *newcontext)
 {
+#ifdef CONFIG_AUDIT
 	struct policydb *policydb = &state->ss->policydb;
 	char *s = NULL, *t = NULL, *n = NULL;
 	u32 slen, tlen, nlen;
@@ -1612,6 +1621,7 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -1929,9 +1939,11 @@ static inline int convert_context_handle_invalid_context(
 	struct selinux_state *state,
 	struct context *context)
 {
+#ifdef CONFIG_AUDIT
 	struct policydb *policydb = &state->ss->policydb;
 	char *s;
 	u32 len;
+#endif
 
 // [ SEC_SELINUX_PORTING_COMMON 
 #ifdef CONFIG_ALWAYS_ENFORCE
@@ -1945,11 +1957,13 @@ static inline int convert_context_handle_invalid_context(
 		return -EINVAL;
 // ] SEC_SELINUX_PORTING_COMMON
 
+#ifdef CONFIG_AUDIT
 	if (!context_struct_to_string(policydb, context, &s, &len)) {
 		pr_warn("SELinux:  Context %s would be invalid if enforcing\n",
 			s);
 		kfree(s);
 	}
+#endif
 	return 0;
 }
 
@@ -1978,7 +1992,9 @@ static int convert_context(u32 key,
 	struct type_datum *typdatum;
 	struct user_datum *usrdatum;
 	char *s;
+#ifdef CONFIG_AUDIT
 	u32 len;
+#endif
 	int rc = 0;
 
 	if (key <= SECINITSID_NUM)
@@ -2093,6 +2109,7 @@ static int convert_context(u32 key,
 out:
 	return rc;
 bad:
+#ifdef CONFIG_AUDIT
 	/* Map old representation to string and save it. */
 	rc = context_struct_to_string(args->oldp, &oldc, &s, &len);
 	if (rc)
@@ -2105,6 +2122,9 @@ bad:
 	       c->str);
 	rc = 0;
 	goto out;
+#else
+    return 0;
+#endif
 }
 
 static void security_load_policycaps(struct selinux_state *state)
@@ -3025,8 +3045,10 @@ int security_sid_mls_copy(struct selinux_state *state,
 	struct context *context1;
 	struct context *context2;
 	struct context newcon;
+#ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
+#endif
 	int rc;
 
 	rc = 0;
@@ -3066,6 +3088,7 @@ int security_sid_mls_copy(struct selinux_state *state,
 	if (!policydb_context_isvalid(policydb, &newcon)) {
 		rc = convert_context_handle_invalid_context(state, &newcon);
 		if (rc) {
+#ifdef CONFIG_AUDIT
 			if (!context_struct_to_string(policydb, &newcon, &s,
 						      &len)) {
 				audit_log(audit_context(),
@@ -3074,6 +3097,7 @@ int security_sid_mls_copy(struct selinux_state *state,
 					  "invalid_context=%s", s);
 				kfree(s);
 			}
+#endif
 			goto out_unlock;
 		}
 	}